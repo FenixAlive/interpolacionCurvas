e13feb76272525b313a46f367413fe7b5b5311e3
AngelLegion <luisangelmf@gmail.com>
2018-10-10 23:04:18 -0500

    listo

--- minimoCuadrado.m
+++ minimoCuadrado.m
@@ -1,9 +1,9 @@
+clear all
+clc
 % problema inventado
-%x = -1:0.1:5;
-%xn = -1:5;
-%f = @(x)(3*x.^3-2*x.^2+x)
-%y = f(x); yn=f(xn)+10*rand(size(xn))-20
-%plot(x,y,'g',xn,yn,'ro');[xn' yn']
+x = -1:0.1:5;
+f = @(x)(3*x.^3-2*x.^2+x);
+y = f(x);
 
 
 xn =[    -1.00000   -21.45398
@@ -17,21 +17,25 @@ xn =[    -1.00000   -21.45398
 %si el modelo es exponencial el yn hacerlo logaritmico pero solo si los 'y' no son negativos
 %si el modelo es de grado n la matriz a es igual a (1 xn xn.^2 xn.^n)
 %para ver el error se saca el error cuadrado medio o la diferencia gaussiana
-
-yn =xn(:,2)
-xn = xn(:,1)
+%k representa el grado del modelo a representar
+% cambiar xn y yn para el problema especifico
+k =2;
+yn =xn(:,2);
+xn = xn(:,1);
 %polinomio grado k
-k =2
-A =[ones(size(xn))]
+A =[ones(size(xn))];
 
 for i=1:k
-  A =[A xn.^i]
+  A =[A xn.^i];
 end
 
 AB = [inv(A'*A)*(A'*yn)]
-% y estimados ir agregando exponenciales
-yest = AB(1)+AB(2)*xn+AB(3)*xn.^2
-% hacer for
+%yest = AB(1)+AB(2)*xn+AB(3)*xn.^2+AB(i+1)*xn.^i;
+yest = AB(1);
+for i=1:(k)
+  yest = yest+AB(i+1)*xn.^i;
+end
 
+plot(x,y,'g',xn,yn,'ro', xn, yest, 'bo');
 % error cuadrado medio
 ECM = mean((yest-yn).^2)
--- newton.m
+++ newton.m
@@ -1,25 +1,30 @@
 clear all
-f = @(x)(1./(1+x.^2)); %función
+clc
+f = @(x)(1./(1+x.^2)); %función real
 a = -2; %punto uno de interpolacion
 b = 2;  %punto dos de interpolacion
 k = 9;  %numero de puntos a considerar
 h = ((b-a)/(k-1)); %tamaño de paso separacion entre cada punto deseado
-xn = a+(0:(k-1))*h  %valores en x
-yn = [f(xn)]  %valores en y
+xn = a+(0:(k-1))*h; %valores en x
+yn = [f(xn)];  %valores en y
 
-x =((a-3):.0001:(b+3))';
+x =((a-1):.001:(b+1))';
 
 A = ones(k,1); %matriz de una columna y tantas filas como puntos
-Ax = ones(size(x),1);
 for i = 1:(k-1)
   A = [A A(:,i).*(xn-xn(i))'];
-  Ax = [Ax Ax(:,i).*(x-x(i))];
 end
 % matriz
 A
 s = inv(A)*yn'
-%Ax = triu(Ax);
-pk =Ax*s;
+% hacer la forma de newton
+pk = s(1);
+xi = ones(size(x-1),1);
+for i = 2:k
+  xi = xi.*(x-xn(i-1));
+  pk = pk + s(i)*xi;
+end
 
-plot(xn,yn,'ro',x,f(x),'b', x, pk, 'r')
-%title("Newton")
+plot(xn,yn,'ro',x,f(x),'b', x,pk,'g--', x, x*0, 'k..');
+axis([a-1,b+1,a-1,b+1]);
+title("Interpolación Newton");
