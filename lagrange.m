5aa8dd9b801dfd1366b55b04d7eebbe669f162a1
FenixAlive <luisangelmf@gmail.com>
2018-10-13 19:11:24 -0500

    agregue datos actividad

--- lagrange.m
+++ lagrange.m
@@ -1,7 +1,10 @@
-x = [1,2,3];
-yn = [9.4,.5,9];
-k=1;
-V = xn(2:3);
-% V = [xn(1:(k-1)),xn((k+1):end)]
-L =poly(V)/prod(xn(1)-V)
-poly2str(L,'L') % no es necesario solo lo hace bonito pero viene de matlab
+xn=  [-2.00  -1.6300  -1.2600 -.8900  -.53  -.16 .21 .58 .95 1.32 1.68 2.05];
+y= [3.5000 4.8700 5.4500 7.7600 7.4000 7.8600 8.7300 8.6700 10.3200 11.0000 10.7000 11.7800];
+
+k=7;
+V = xn(2:end);
+
+V = [xn(1:(k-1)),xn((k+1):end)]
+Lt=(poly(V)/prod(xn(k)-V))
+%L =poly(V)/prod(xn(1)-V);
+poly2str(Lt,'x') % no es necesario solo lo hace bonito pero viene de matlab
--- newton.m
+++ newton.m
@@ -3,11 +3,12 @@ clc
 f = @(x)(1./(1+x.^2)); %función real
 a = -2; %punto uno de interpolacion
 b = 2;  %punto dos de interpolacion
-k = 9;  %numero de puntos a considerar
+k = 12;  %numero de puntos a considerar
 h = ((b-a)/(k-1)); %tamaño de paso separacion entre cada punto deseado
-xn = a+(0:(k-1))*h; %valores en x
-yn = [f(xn)];  %valores en y
-
+%xn = a+(0:(k-1))*h; %valores en x
+%yn = [f(xn)];  %valores en y
+xn=  [-2.00  -1.6300  -1.2600 -.8900  -.53  -.16 .21 .58 .95 1.32 1.68 2.05];
+yn= [3.5000 4.8700 5.4500 7.7600 7.4000 7.8600 8.7300 8.6700 10.3200 11.0000 10.7000 11.7800];
 x =((a-1):.001:(b+1))';
 
 A = ones(k,1); %matriz de una columna y tantas filas como puntos
